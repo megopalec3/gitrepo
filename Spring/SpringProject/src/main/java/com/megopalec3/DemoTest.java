7e37fa694022a6c16d8d7d8761c9771e36f18cd5
Oleg Skorykov <Oleg_Skorykov@epam.com>
2016-04-01 16:33:09 +0300

    TODO

--- Spring/SpringProject/src/main/java/com/megopalec3/DemoTest.java
+++ Spring/SpringProject/src/main/java/com/megopalec3/DemoTest.java
@@ -4,7 +4,7 @@ import com.megopalec3.dao.UserDAO;
 import com.megopalec3.entity.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+//TODO: Remove class
 public class DemoTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
