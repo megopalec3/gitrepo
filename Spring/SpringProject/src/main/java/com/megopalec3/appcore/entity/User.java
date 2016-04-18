a420ab52f32788f35ea333d3e0f7b360fae65bef
Oleg Skorykov <Oleg_Skorykov@epam.com>
2016-04-18 16:12:35 +0300

    Setters for User interface

--- Spring/SpringProject/src/main/java/com/megopalec3/appcore/entity/User.java
+++ Spring/SpringProject/src/main/java/com/megopalec3/appcore/entity/User.java
@@ -2,10 +2,13 @@ package com.megopalec3.appcore.entity;
 
 public interface User {
 
+    User setPassword(String password);
     String getPassword();
 
+    User setId(long id);
     long getId();
 
     String getUserName();
+    User setUserName(String userName);
 
 }
